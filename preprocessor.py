4f2c8f762c351f237b6a22d10a9a1114a3c97aa7
tanya <117272817+taya12hu@users.noreply.github.com>
2024-12-29 22:25:50 +0530

    Initial commit

--- preprocessor.py
+++ preprocessor.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import re
 import pandas as pd
 
@@ -20,28 +19,3 @@ def preprocess(data):
 
     # Display the DataFrame
     return df
-
-=======
-import re
-import pandas as pd
-
-def preprocess(data):
-    pattern = r'(\d{2}/\d{2}/\d{2}), (\d{2}:\d{2}) - (.*?): (.*)'
-
-    # Split the data into lines
-    lines = data.split('\n')
-
-    # Parse lines into lists
-    parsed_data = []
-    for line in lines:
-        match = re.match(pattern, line)
-        if match:
-            parsed_data.append(match.groups())
-
-    # Convert to DataFrame
-    df = pd.DataFrame(parsed_data, columns=['Date', 'Time', 'Sender', 'Message'])
-
-    # Display the DataFrame
-    return df
-
->>>>>>> 7b42a8b7034339b285df1333cc77fd312f0ffce3
